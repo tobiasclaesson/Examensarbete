6dd27e8c13149d9a70992c40327bfea84d445d58
Tobias Claesson <tobbe.classon@hotmail.se>
2021-05-04 09:08:56 +0200

    first commit

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKyuRY2pBXn0BoIlkH5EV2t_Oe6F28d7A',
+  authDomain: 'prioritization-app.firebaseapp.com',
+  projectId: 'prioritization-app',
+  storageBucket: 'prioritization-app.appspot.com',
+  messagingSenderId: '376121967400',
+  appId: '1:376121967400:web:74070126c24282d576061d',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+export const db = firebase.firestore();
