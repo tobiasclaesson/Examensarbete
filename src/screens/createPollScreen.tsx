68c9d69cf8abc4ab4b89bbf798b3d98143b2483a
Tobias Claesson <tobbe.classon@hotmail.se>
2021-05-07 16:29:53 +0200

    added list of items, add button function, updatePoll function

--- src/screens/createPollScreen.tsx
+++ src/screens/createPollScreen.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useContext, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import { AppStackParamList } from '../navigation/appStack';
 import { StackNavigationProp } from '@react-navigation/stack';
 import colors from '../utils/colors';
 
-import { Button, TextInputField } from '../components';
+import { Button, PollListItem, TextInputField } from '../components';
 import { DBContext } from '../context/dbContext';
 import { IPoll } from '../utils/types';
 import { ScrollView } from 'react-native-gesture-handler';
+import { updatePoll } from '../store/actions';
+import { checkArrayForDuplicates } from '../utils/common';
 
 type CreatePollNavigationProp = StackNavigationProp<
   AppStackParamList,
@@ -22,32 +24,104 @@ const MainScreen: FC<IProps> = (props: IProps) => {
   const { navigation } = props;
 
   const { addPoll } = useContext(DBContext);
-  const [newPoll, setNewPoll] = useState<IPoll>({
-    title: '',
-    options: [],
-    usersHaveVoted: [],
-    answers: [],
-  });
+
+  const [title, setTitle] = useState<string>('');
+  const [options, setOptions] = useState<string[]>([]);
+
+  console.log(options);
+
+  const addOption = () => {
+    setOptions([...options, '']);
+  };
+  const setOption = (text: string, optionIndex: number) => {
+    setOptions((prevState) => {
+      const opt = prevState.map((option, i) => {
+        if (optionIndex === i) {
+          return text;
+        } else {
+          return option;
+        }
+      });
+      return opt;
+    });
+  };
+
+  enum EAlertTypes {
+    Title = 'TITLE',
+    MissingOptions = 'MISSING_OPTIONS',
+    DuplicateOptions = 'DUPLICATE_OPTIONS',
+  }
+  const createAlert = (type: string) => {
+    let msg = '';
+    switch (type) {
+      case EAlertTypes.Title: {
+        msg = 'There are empty titles, please correct before updating poll';
+        break;
+      }
+      case EAlertTypes.MissingOptions: {
+        msg =
+          'There are to few options, please add options before updating poll';
+        break;
+      }
+      case EAlertTypes.DuplicateOptions: {
+        msg =
+          'There is a duplicate in options, please correct before updating poll';
+        break;
+      }
+    }
+    Alert.alert('Error', msg, [{ text: 'OK' }]);
+  };
+  const updatePoll = () => {
+    if (options.length < 2) {
+      createAlert(EAlertTypes.MissingOptions);
+      return;
+    }
+    if (title === '') {
+      createAlert(EAlertTypes.Title);
+      return;
+    }
+    let optionTitleEmpty = false;
+    options.forEach((option) => {
+      if (option === '') {
+        optionTitleEmpty = true;
+      }
+    });
+    if (optionTitleEmpty) {
+      createAlert(EAlertTypes.Title);
+      return;
+    }
+    if (checkArrayForDuplicates(options)) {
+      createAlert(EAlertTypes.DuplicateOptions);
+      return;
+    }
+
+    addPoll(
+      { title: title, options: options, usersHaveVoted: [], answers: [] },
+      () => navigation.goBack()
+    );
+  };
 
   return (
     <View style={styles.container}>
       <TextInputField
         placeholder='title'
-        value={newPoll.title}
-        onChangeText={(text) => setNewPoll({ ...newPoll, title: text })}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
       />
       <View style={styles.scrollViewContainer}>
-        <ScrollView style={styles.scrollView}></ScrollView>
+        <ScrollView style={styles.scrollView}>
+          {options.map((option, i) => (
+            <PollListItem
+              key={i}
+              title={option}
+              onChangeText={(text) => setOption(text, i)}
+            />
+          ))}
+        </ScrollView>
       </View>
       <View style={styles.buttonsContainer}>
-        <Button
-          title='Add option'
-          onPress={() => console.log('adding option')}
-        />
-        <Button
-          title='Create and Replace Poll'
-          onPress={() => console.log('updating poll')}
-        />
+        <Button title='Add option' onPress={() => addOption()} />
+        <Button title='Create and Replace Poll' onPress={() => updatePoll()} />
       </View>
     </View>
   );
@@ -59,6 +133,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 10,
     height: '100%',
+    backgroundColor: colors.lightGrey,
   },
   scrollViewContainer: {
     paddingTop: 10,
