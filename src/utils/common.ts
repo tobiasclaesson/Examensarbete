2454e28cc718b03208ef620ee0dd1e625ed1ec18
Tobias Claesson <tobbe.classon@hotmail.se>
2021-05-11 11:44:51 +0200

    updated duplicate function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,11 @@
 import { IOption } from './types';
 
 export function checkArrayForDuplicates(arr: IOption[]): boolean {
-  return new Set(arr).size !== arr.length;
+  const array: string[] = [];
+  arr.forEach((a) => {
+    array.push(a.title);
+  });
+  console.log(new Set(array).size !== array.length);
+
+  return new Set(array).size !== array.length;
 }
