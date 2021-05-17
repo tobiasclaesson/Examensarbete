efbcbb7703fbf7ab04dfde30b65ddc875a1b482d
Tobias Claesson <tobbe.classon@hotmail.se>
2021-05-17 11:53:59 +0200

    added background

--- src/screens/splashScreen.tsx
+++ src/screens/splashScreen.tsx
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: colors.lightGrey,
   },
   title: {
     fontFamily: 'Verdana',
